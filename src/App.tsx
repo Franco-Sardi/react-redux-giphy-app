7a00c18e6debbed4dea3b0213b6ea441b28c4eaa
FrancoUtn <francosardiutn@gmail.com>
2025-05-14 23:45:14 -0300

    Instalo redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { GiftList } from "./components/GiftLista/GiftList"
 import { NavBar } from "./components/ui/NavBar/NavBar"
 
-interface IGift {
-  urlGift: string,
-  title:string
-}
 function App() {
   return (
     <>
